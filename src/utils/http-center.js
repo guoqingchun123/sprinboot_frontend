c80c1da587374fe01353dfcbcd07417efa38bab6
liujiankai <liujiankai@bestvike.com>
2019-12-26 10:48:46 +0800

    更新http版本

--- src/utils/http-center.js
+++ src/utils/http-center.js
@@ -6,15 +6,19 @@ import createInstance from '@bestvike/utils/lib/http'
 const instance = createInstance({
   baseURL: process.env.VUE_APP_MOCK === 'true' ? '' : process.env.VUE_APP_ROOT,
   appCode: process.env.VUE_APP_CODE,
-  router: router,
-  store: store,
-  configUrl: (url) => {
+  init() {
+    return {
+      router,
+      store
+    }
+  },
+  configUrl(url) {
     if (process.env.VUE_APP_CENTER_API !== '/' && url && url.indexOf(process.env.VUE_APP_CENTER_API) !== 0) {
       return process.env.VUE_APP_CENTER_API + url
     }
     return url
   },
-  showMessage: (options) => {
+  showMessage(options) {
     Message(options)
   }
 })
